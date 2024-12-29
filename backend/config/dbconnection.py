3c87ecda083994f6bf0d302408306343d0ff8581
rajpopzein <kraj0132@gmail.com>
2024-12-29 18:01:00 +0530

    added docker network

--- backend/config/dbconnection.py
+++ backend/config/dbconnection.py
@@ -1,9 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 
-dataBaseUrl = "mysql+pymysql://root:1234567@127.0.0.1:3306/userManagement"
+# dataBaseUrl = "mysql+pymysql://root:1234567@127.0.0.1:3306/userManagement"
+
+print(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/userManagement", "======")
+
+dataBaseUrl = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/userManagement"
 
 engine = create_engine(dataBaseUrl, pool_size=20, max_overflow=0)
 
