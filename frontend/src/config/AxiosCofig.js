aa9d64b65f50492c5ddf0580735cd6396d9efb45
Rajkumar R <ravindran.rajkumar@vectramind.com>
2025-02-08 22:57:50 +0530

    updated code with server url

--- frontend/src/config/AxiosCofig.js
+++ frontend/src/config/AxiosCofig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL:'http://43.204.150.47:8000',
-  baseURL:'http://localhost:8000',
+  baseURL:'http://43.204.150.47:8000',
+  // baseURL:'http://localhost:8000',
   timeout:5000
 })
 
